c0ecb277ce5d0f4e89138eb2841de4cdcc1dffc0
bonzer75 <haroldveracs@gmail.com>
2022-10-14 21:40:01 -0500

    i don't need this code

--- commands/fun/rate.js
+++ commands/fun/rate.js
@@ -3,7 +3,7 @@ module.exports = (client, message, args, config) => {
 		if (sub === client.user) return message.channel.send("Jaja, no");
 	let err = args.slice().join(" ")
 		if (!err) return message.channel.send("Eh?")
-		
+
 	let num = ["1/5 :thumbsdown:", "2/5 :thumbsdown:", "3/5 :thumbsup:", "4/5 :thumbsup:", "5/5 :ok_hand:"]
 	let rate = num[Math.floor(Math.random() * num.length)]
 
@@ -12,14 +12,6 @@ module.exports = (client, message, args, config) => {
 			color: 2292676,
 			title: `En respuesta a ${message.author.username}`,
 			description: `Hmmm, yo le doy a **${err}** un ${rate}`
-		}
-	});
-
-	if (sub.id === config.owner[1] || sub.id === config.wife[1]) return message.channel.send({
-		embed: {
-			color: 2292676,
-			title: `En respuesta a ${message.author.username}`,
-			description: `Hmmm, yo le doy a **${sub.username}** un 5/5 :ok_hand:`
 		}
 	});
 
