a8d2ea4084e10a2bc5565530fbef30a688839735
bonzer75 <haroldveracs@gmail.com>
2023-01-02 20:16:16 -0500

    better try this

--- commands/utility/report.js
+++ commands/utility/report.js
@@ -3,27 +3,13 @@ const { MessageEmbed } = require('discord.js')
 require("dotenv").config()
 
 module.exports = (client, message, args) => {
-    const server = message.guild;
-
-    let report = args.join(' ');
-	if(!report) return
 
     let embed = new Discord.MessageEmbed()
-        .setTitle('Padre, tienes un mensaje nuevo')
-   	    .setDescription(report)
+        .setTitle('Cómo reportar un bug')
+   	    .setDescription("Gracias por tu apoyo e interés, pero necesitamos que nos digas que pasó, puedes hacer el reporte en Github y mi padre lo verá")
   	    .setColor(0xe51a4c)
-   	    .addField('Servidor', `${server.name} __${server.id}__`)
-        .addField('Usuario', `${message.author.tag} __${message.author.id}__`)
+        .addField('Este es el link:', `https://github.com/bonzer75/ash-best-bot/issues`)
 
-    function sendMessage(embed) {
-        client.users.cache.get(process.env.OWNER).send(embed)
-    }
-
-    try {
-        sendMessage(embed)
-    } catch (err) {
-        message.channel.send("Algo salió muy mal... ```" + err + "``` ");
-        console.log("Something went wrong")
-    }
+    message.channel.send("Algo salió muy mal... ```" + err + "``` ");
    //you need to allow direct server messages... i should rework this later
 }
